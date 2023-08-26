6dd18b6afcf52dfbf572efef0543dc43e3a2c005
profthomasvilches <thomas.vilches@unesp.br>
2023-08-26 10:20:29 -0300

    segunda aula regex

--- Regex/Regex.R
+++ Regex/Regex.R
@@ -3,7 +3,7 @@
 
 library(tidyverse)
 library(pdftools)
-
+library(lubridate)
 
 
 # Para checar -------------------------------------------------------------
@@ -92,6 +92,99 @@ y <- gsub("\\(\\w+ |\\)", "", y)
 
 y <- pdf[grepl("[nN]ome:", pdf)]
 y <- str_extract(y, "(?<=\\(aka ).*(?=\\))")
+
+# (?<=\\(aka )
+# (?=\\))
+# str_extract("Thomas", "o.a")
+
+# CEP
+
+z <- pdf[grepl("[Cc][Ee][pP]", pdf)]
+
+z <- gsub(".*CEP: *", "", z)
+z <- gsub("\\D", "", z)
+
+z1 <- str_extract(z, "^\\d{5}")
+z2 <- str_extract(z, "\\d{3}$")
+
+z <- paste(z1, z2, sep = "-")
+
+
+z <- pdf[grepl("[Cc][Ee][pP]", pdf)]
+
+z <- str_extract(z, "(?<=CEP: )[\\d\\.\\-]+$")
+
+z <- gsub("\\D", "", z)
+z1 <- str_extract(z, "^\\d{5}")
+z2 <- str_extract(z, "\\d{3}$")
+
+z <- paste(z1, z2, sep = "-")
+
+#CEP: 13000-000
+
+
+# CPF: ??
+
+cpf <- pdf[grepl("cpf:|CPF:", pdf)]
+
+cpf <- gsub(".*[cC][pP][fF]: *", "", cpf)
+cpf <- gsub("\\D", "", cpf)
+
+
+# Tel: ??
+
+tel <- pdf[grepl("Tel:?", pdf)]
+tel <- gsub("\\D", "", tel)
+
+
+tel1 <- str_extract(tel, "^\\d{2}")
+tel2 <- str_extract(tel, "(?<=\\d{2})\\d{5}(?=\\d{4})")
+tel3 <- str_extract(tel, "(?<=\\d{7})\\d{4}")
+
+tel <- paste0("(", tel1, ") ", tel2, "-", tel3)
+
+
+# Versão que o Maicon explicou
+# Ótima solução
+
+telefone <- pdf[grepl("Tel:|Telefone:",pdf)]
+telefone <- gsub("Tel:|Telefone:|[-\\(\\)]","",telefone)
+telefone <- gsub("\\s","",telefone)
+telefone <- gsub("^(\\d{2})","(\\1)",telefone)
+telefone <- gsub("^(.{9})", "\\1-", telefone)
+
+# Data de nascimento
+
+
+dt <- pdf[grepl("Da?ta? (de )?[Nn]asc",pdf)]
+
+dt <- gsub(".*:", "", dt)
+
+obs_dt <- str_extract(dt, "[A-Za-z]+\\.[A-Za-z]+\\.?$")
+
+dt <-trimws(dt, "both")
+
+dt <- str_extract(dt, "\\d+[-/]\\d+[-/]\\d+(\\.\\d+)?|\\d+[-/]\\w+[-/]\\d+")
+dt <- gsub("\\.", "", dt)
+
+dt <- gsub("([-/])(\\d{3})$", "\\10\\2",dt)
+# 
+# dt_teste <- c("12-12-281", "12/12/281")
+# gsub("[-/](\\d{3})$", "-0\\1",dt_teste)
+# 
+# gsub("([-/])(\\d{3})$", "\\10\\2",dt_teste)
+
+
+# tarefa - organizar o endereço
+# Rua/Local
+# numero
+# Cidade/Planeta
+
+dt_char <- dt
+
+dt <- dmy(dt)
+
+dmy("01/01/0900") %>% typeof()
 
 # Teste exemplo
 
